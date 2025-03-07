7cc778c6846a640e8395dbbadcfeee6850eaf7f3
Yassine <yassineelbrahmi11@gmail.com>
2025-03-07 16:09:24 +0100

    Add BaseDbContext for multi-tenancy support
    Introduces a new abstract class `BaseDbContext` that extends

---
+++ Infrastructure/Contexts/BaseDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Finbuckle.MultiTenant;
+using Finbuckle.MultiTenant.Abstractions;
+using Finbuckle.MultiTenant.EntityFrameworkCore;
+using Infrastructure.Identity.Models;
+using Infrastructure.Tenancy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Contexts
+{
+    public abstract class BaseDbContext : MultiTenantIdentityDbContext<
+        ApplicationUser,
+        ApplicationRole,
+        string,
+        IdentityUserClaim<string>,
+        IdentityUserRole<string>,
+        IdentityUserLogin<string>,
+        ApplicationRoleCLaim,
+        IdentityUserToken<string>
+        >
+    {
+        private new SchoolTenantInfo TenantInfo { get; set; }
+
+        protected BaseDbContext(IMultiTenantContextAccessor<TenantInfo> tenantContextAccessor, DbContextOptions options) : base(tenantContextAccessor, options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (!string.IsNullOrEmpty(TenantInfo?.ConnectionString))
+            {
+                optionsBuilder.UseSqlServer(TenantInfo.ConnectionString, options => 
+                    options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName));
+            }
+        }
+    }
+}
