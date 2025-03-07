8e8bee0a9436bbddde7aa641b374fe00d1e6a2cc
Yassine <yassineelbrahmi11@gmail.com>
2025-03-07 16:10:05 +0100

    Add tenant management classes and DbContext configuration

---
+++ Infrastructure/Tenancy/SchoolTenantInfo.cs
@@ -0,0 +1,22 @@
+﻿using Finbuckle.MultiTenant.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Tenancy
+{
+    public class SchoolTenantInfo : ITenantInfo
+    {
+        public string Id { get; set; }
+        public string Identifier { get; set; }
+        public string Name { get; set; }
+        public string ConnectionString { get; set; }
+        public string Email { get; set; }
+        public string Firsttname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime ValidUntil { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
---
+++ Infrastructure/Tenancy/TenantDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Finbuckle.MultiTenant.EntityFrameworkCore.Stores.EFCoreStore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Tenancy
+{
+    public class TenantDbContext(DbContextOptions<TenantDbContext> options) :
+        EFCoreStoreDbContext<SchoolTenantInfo>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SchoolTenantInfo>()
+                .ToTable("Tenants", "Multitenancy");
+        }
+    }
+}
